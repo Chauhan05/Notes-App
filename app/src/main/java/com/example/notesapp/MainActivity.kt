25f6d3fdb868b3cc6284b66a49b3dd737573a1bc
Nitin Chauhan <nitinchauhan0808@gmail.com>
2025-06-05 14:56:01 +0530

    animation

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.example.notesapp
 
+import AppNavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
-import com.example.notesapp.navigation.AppNavigation
 import com.example.notesapp.ui.theme.NotesAppTheme
 import com.example.notesapp.viewmodels.AppViewModel
 
@@ -24,9 +16,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val viewModel: AppViewModel = viewModel()
-            val navController= rememberNavController()
+            val navController = rememberNavController()
             NotesAppTheme(darkTheme = true) {
-                AppNavigation(viewModel=viewModel, navController = navController)
+                AppNavigation(viewModel = viewModel, navController = navController)
 //                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 ////                    AppNavigation(modifier=Modifier.padding(innerPadding), viewModel = viewModel, navController =navController )
 //                }
--- app/src/main/java/com/example/notesapp/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/notesapp/navigation/AppNavigation.kt
@@ -1,5 +1,13 @@
-package com.example.notesapp.navigation
-
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -7,11 +15,12 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import com.example.notesapp.viewmodels.AppViewModel
+import com.example.notesapp.navigation.Screens
 import com.example.notesapp.screens.AddNoteScreen
 import com.example.notesapp.screens.HomeScreen
 import com.example.notesapp.screens.UpdateNoteScreen
 import com.example.notesapp.screens.ViewNoteScreen
+import com.example.notesapp.viewmodels.AppViewModel
 
 @Composable
 fun AppNavigation(
@@ -19,49 +28,86 @@ fun AppNavigation(
     viewModel: AppViewModel,
     navController: NavHostController
 ) {
+    val duration = 800 // shorter, snappier animation
+    val easing = androidx.compose.animation.core.FastOutSlowInEasing
 
-    NavHost(navController, startDestination = Screens.HomeScreen.route) {
-        composable(Screens.HomeScreen.route) {
-            HomeScreen(navController,viewModel)
-        }
-        composable(Screens.AddNoteScreen.route) {
-            AddNoteScreen(navController,viewModel)
-        }
-//        composable(Screens.UpdateNoteScreen.route) {backStackEntry->
-//            val noteId = backStackEntry.arguments?.getString("noteId")?.toIntOrNull()
-//            if (noteId != null) {
-//                UpdateNoteScreen(noteId, navController, viewModel)
-//            }
-//        }
-
-        composable(
-            route = "${Screens.UpdateNoteScreen.route}/{noteId}",
-            arguments = listOf(navArgument("noteId"){
-                type= NavType.IntType
-                nullable=false
-            })
-        ) { backStackEntry ->
-            val noteId = requireNotNull(backStackEntry.arguments?.getInt("noteId")) {
-                "Note ID is required for UpdateNoteScreen"
-            }
-            UpdateNoteScreen(noteId, navController, viewModel)
-
-        }
-
-
-        composable(
-            route = "${Screens.ViewNote.route}/{noteId}",
-            arguments = listOf(navArgument("noteId"){
-                type= NavType.IntType
-                nullable=false
-            })
-        ) { backStackEntry ->
-            val noteId = requireNotNull(backStackEntry.arguments?.getInt("noteId")) {
-                "Note ID is required for UpdateNoteScreen"
-            }
-            ViewNoteScreen(noteId, navController, viewModel)
-
-        }
+    val enterForward: AnimatedContentTransitionScope<*>.() -> EnterTransition = {
+        slideIntoContainer(
+            AnimatedContentTransitionScope.SlideDirection.Left,
+            animationSpec = tween(durationMillis = duration, easing = easing)
+        ) + fadeIn(animationSpec = tween(durationMillis = duration, easing = easing))
     }
 
+    val exitForward: AnimatedContentTransitionScope<*>.() -> ExitTransition = {
+        slideOutOfContainer(
+            AnimatedContentTransitionScope.SlideDirection.Left,
+            animationSpec = tween(durationMillis = duration, easing = easing)
+        ) + fadeOut(animationSpec = tween(durationMillis = duration, easing = easing))
+    }
+
+    val enterBackward: AnimatedContentTransitionScope<*>.() -> EnterTransition = {
+        slideIntoContainer(
+            AnimatedContentTransitionScope.SlideDirection.Right,
+            animationSpec = tween(durationMillis = duration, easing = easing)
+        ) + fadeIn(animationSpec = tween(durationMillis = duration, easing = easing))
+    }
+
+    val exitBackward: AnimatedContentTransitionScope<*>.() -> ExitTransition = {
+        slideOutOfContainer(
+            AnimatedContentTransitionScope.SlideDirection.Right,
+            animationSpec = tween(durationMillis = duration, easing = easing)
+        ) + fadeOut(animationSpec = tween(durationMillis = duration, easing = easing))
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ){
+        NavHost(navController, startDestination = Screens.HomeScreen.route) {
+            composable(
+                Screens.HomeScreen.route,
+                enterTransition = { enterBackward() },
+                exitTransition = { exitForward() }
+            ) {
+                HomeScreen(navController, viewModel)
+            }
+            composable(
+                Screens.AddNoteScreen.route,
+                enterTransition = { enterForward() },
+                exitTransition = { exitBackward() }
+            ) {
+                AddNoteScreen(navController, viewModel)
+            }
+            composable(
+                route = "${Screens.UpdateNoteScreen.route}/{noteId}",
+                arguments = listOf(navArgument("noteId") {
+                    type = NavType.IntType
+                    nullable = false
+                }),
+                enterTransition = { enterForward() },
+                exitTransition = { exitBackward() }
+            ) { backStackEntry ->
+                val noteId = requireNotNull(backStackEntry.arguments?.getInt("noteId")) {
+                    "Note ID is required for UpdateNoteScreen"
+                }
+                UpdateNoteScreen(noteId, navController, viewModel)
+            }
+
+            composable(
+                route = "${Screens.ViewNote.route}/{noteId}",
+                arguments = listOf(navArgument("noteId") {
+                    type = NavType.IntType
+                    nullable = false
+                }),
+                enterTransition = { enterForward() },
+                exitTransition = { exitBackward() }
+            ) { backStackEntry ->
+                val noteId = requireNotNull(backStackEntry.arguments?.getInt("noteId")) {
+                    "Note ID is required for ViewNoteScreen"
+                }
+                ViewNoteScreen(noteId, navController, viewModel)
+            }
+        }
+    }
 }
